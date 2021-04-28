d3b2723b52c960411649264b0bbff0815822bf2b
tuana8tmtt <tuana8tmt@gmail.com>
2021-04-28 10:44:40 +0700

    Extract words

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
-namespace vitalis\HelloWorld;
+require_once __DIR__ . '/vendor/autoload.php';
 
-class Index
-{
-    public function greet($greet = "Hello World")
-    {
-        return $greet;
-    }
-}
+use tuana8tmt\TextExtract\Index;
+
+$greeting = new Index();
+$return = $greeting->extract("Keyword extraction is not that difficult after all. There are many libraries that can help you with keyword extraction. Rapid automatic keyword extraction is one of those.", __DIR__ ."/stop_words.txt");
+print_r($return);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7b6f6744dca1f021c7e39bdd224add29be872951',
+    'name' => 'tuana8tmt/rake_text',
+  ),
+  'versions' => 
+  array (
+    'tuana8tmt/rake_text' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7b6f6744dca1f021c7e39bdd224add29be872951',
+    ),
+  ),
+);
